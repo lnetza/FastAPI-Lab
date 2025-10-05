ee6b4e911176349914e9b377e7af4a1b4be33f26
netza <l.lopeznetza@gmail.com>
2025-10-04 22:44:24 -0600

    request_response_body

--- API Lab1/main.py
+++ API Lab1/main.py
@@ -1,9 +1,22 @@
+from typing import Optional
+from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
 
+#Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get("/")
-
 def home():
     return {"url": "https://www.google.com"}
+
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
